3a8e500a7db3cbaffdc4f395ce1989ba131ff77f
Leo Gbojueh <lgbojueh@msudenver.edu>
2025-04-10 10:24:21 -0600

    Commit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,11 @@
-// src/app/layout.tsx
+"use client";
+
 import type { Metadata } from "next";
 import React from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react"; // Import SessionProvider
 import ClientNavbar from "../components/ClientNavbar";
 
 export const metadata: Metadata = {
@@ -31,19 +33,16 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{String(metadata.title)}</title>
-        <meta
-          name="description"
-          content={metadata.description || "Find the perfect board game"}
-        />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system">
-          <ClientNavbar />
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            {children}
-          </div>
-        </ThemeProvider>
+        <SessionProvider> {/* Wrap the application with SessionProvider */}
+          <ThemeProvider attribute="class" defaultTheme="system">
+            <ClientNavbar />
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              {children}
+            </div>
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
