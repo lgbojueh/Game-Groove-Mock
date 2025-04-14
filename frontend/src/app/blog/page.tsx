702b539d1237e841b2449041193b2fed1c46d2ba
Leo Gbojueh <lgbojueh@msudenver.edu>
2025-04-13 23:43:21 -0600

    Commit

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -44,12 +44,10 @@ export default function Blog() {
           >
             {/* Post Header */}
             <header>
-              <Link href={`/blog/${post.id}`}>
-                <a aria-label={`Read more about ${post.title}`}>
+              <Link href={`/blog/${post.id}`}> aria-label={`Read more about ${post.title}`}{">"}
                   <h2 className="text-2xl font-semibold mb-2 hover:underline">
                     {post.title}
                   </h2>
-                </a>
               </Link>
             </header>
 
