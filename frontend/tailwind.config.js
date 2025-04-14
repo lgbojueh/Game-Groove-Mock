8b56ab3e9aa48669890e95823148becc2487147d
Leo Gbojueh <lgbojueh@msudenver.edu>
2025-04-13 19:05:10 -0600

    Commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', // Include all JS/TS/React files
-    './public/**/*.html',         // Include any HTML files in the public folder
+    "./src/app/**/*.{js,ts,jsx,tsx}", 
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
